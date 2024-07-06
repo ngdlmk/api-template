2e1cd5eab080f7f0981f36e0f5173a4ef68c72a5
Kamil Dogan <kamildogan@Kamils-MBP.home>
2024-07-07 01:42:42 +0300

    test format

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import AuthController from 'controllers/auth-controller';
 const router = express.Router();
-
+const name = 'kamil dogan';
+console.log(name);
 router.post('/create-account', AuthController.createUser);
 router.post('/login', AuthController.login);
 export default router;
